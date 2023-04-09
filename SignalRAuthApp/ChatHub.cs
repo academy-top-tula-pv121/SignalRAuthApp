e3d3ec764886d8d41a569a2993527ebeb1179955
maxefimov <efimov.top.academy@gmail.com>
2023-04-09 18:29:29 +0300

    Добавьте файлы проекта.

---
+++ SignalRAuthApp/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRAuthApp
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message)
+        {
+            await Clients.All.SendAsync("Receive", username, message);
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task Notify(string message)
+        {
+            await Clients.All.SendAsync("Receive", "Admin", message);
+        }
+    }
+}
